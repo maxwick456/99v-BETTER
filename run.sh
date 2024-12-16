78fae5b27edf83c9ad1edb8f8f480be8073273b3
Max <maxwick456@gmail.com>
2024-12-15 23:23:11 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
 for x in {1..100000}; do time curl https://script.google.com/macros/s/AKfycbx1xYwL2QqkOQHyeCWIVSMVLP_XXts5zpb0M-vbvlRl1MpRYYcv4NI-i8OaGc0YP0wpsg/exec?title=99v.org%20BETTER
- ; done ;
